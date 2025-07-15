09bf641420bc504a05444306fead7ffadd33d6d9
varun utukuri <varun.utukuri@gmail.com>
2025-07-15 16:45:28 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ import dill
 import pickle
 from exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV 
 
 def save_object(file_path, obj):
     try:
@@ -23,24 +24,29 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_key in models.keys():
+            model = models[model_key]
+            if model_key not in params:
+                raise ValueError(f"Model key '{model_key}' not found in params dictionary.")
+            params_for_model = params[model_key]
 
-            model.fit(X_train,y_train)
+            gs = GridSearchCV(model, params_for_model, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_key] = test_model_score
 
         return report
 
