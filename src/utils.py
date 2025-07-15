1f02090729abf25c65ce45d25499f82ef27591f4
varun utukuri <varun.utukuri@gmail.com>
2025-07-16 00:30:00 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e, error_detail=sys)
 
 
 def evaluate_models(X_train, y_train,X_test,y_test,models,params):
@@ -51,5 +51,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,params):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)
-    
+        raise CustomException(e, error_detail=sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, error_detail=sys)
